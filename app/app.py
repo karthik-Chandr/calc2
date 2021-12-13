a4e90504b5ce7ac0c7d69e5cead28d5059068b1f
karthik <kc69@njit.edu>
2021-12-12 21:54:00 -0500

    Final commit

---
+++ app/app.py
@@ -0,0 +1,29 @@
+"""A simple flask web app"""
+from flask import Flask
+from app.controllers.index_controller import IndexController
+from app.controllers.tablecontroller import TableController
+from app.controllers.oop import OOPController
+from app.controllers.calculator_controller import CalculatorController
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route("/", methods=['GET'])
+def index_get():
+    return IndexController.get()
+
+@app.route("/calculator", methods=['GET'])
+def calculator_get():
+    return CalculatorController.get()
+
+@app.route("/table", methods=['GET'])
+def table_get():
+    return TableController.get()
+
+@app.route("/oops_articles", methods=['GET'])
+def oop_get():
+    return OOPController.get()
+
+@app.route("/calculator", methods=['POST'])
+def calculator_post():
+    return CalculatorController.post()
---
+++ readme.md
@@ -0,0 +1,30 @@
+# Calculator Project 3
+
+### -Created by Neha Kazi, Shilpa Parameshwara and Karthik Chandrashekar
+
+### [Calculator](https://youtu.be/WfqhQGAwGXc)
+
+#Calculator home page
+
+![home page](web_screenshot/Screenshot%20(28).png)
+#addition page
+![addition page](web_screenshot/Screenshot%20(29).png)
+#addition result
+![addition result page](web_screenshot/Screenshot%20(30).png)
+#flash messages
+![flash page](web_screenshot/Screenshot%20(31).png)
+#subtraction result
+![sub page](web_screenshot/Screenshot%20(34).png)
+#mult result
+![mult page](web_screenshot/Screenshot%20(36).png)
+#div result
+![div page](web_screenshot/Screenshot%20(38).png)
+#tables
+![table page](web_screenshot/Screenshot%20(39).png)
+#oops 
+![oops page](web_screenshot/Screenshot%20(40).png)
+
+
+
+
+
